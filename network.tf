b88f4a5ab918e9a88f4bd0f8220e8493e401f2a7
Mohammed Abuteir <moh2015moh21415@gmail.com>
2026-07-29 16:13:13 +0300

    assoc routing table for other subnets

--- network.tf
+++ network.tf
@@ -92,8 +92,18 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public_route.id
 }
 
+resource "aws_route_table_association" "public_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route.id
+}
+
 resource "aws_route_table_association" "private_assoc" {
   subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route.id
+}
+
+resource "aws_route_table_association" "private_assoc_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
   route_table_id = aws_route_table.private_route.id
 }
 
